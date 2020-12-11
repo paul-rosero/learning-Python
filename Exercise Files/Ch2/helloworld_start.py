1343caac1e6c5265fc5b1ca4b2f26eb1e6c6199b
Paul Rosero <paul.rosero23@gmail.com>
2020-12-11 14:16:47 -0500

    create a function that prints out the same as commented out code

--- Exercise Files/Ch2/helloworld_start.py
+++ Exercise Files/Ch2/helloworld_start.py
@@ -6,4 +6,11 @@
 # name = input("What is your name? ")
 # print("Nice to meet you,", name)
 
+def main():
+    print("hello world!")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name)
+
+if __name__ == "__main__":
+    main()
 
