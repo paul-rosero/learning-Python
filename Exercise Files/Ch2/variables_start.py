496f6a750c730a332bf5ed272de6cfe147273479
Paul Rosero <paul.rosero23@gmail.com>
2020-12-12 17:52:20 -0500

    re-declare the f variable to print it

--- Exercise Files/Ch2/variables_start.py
+++ Exercise Files/Ch2/variables_start.py
@@ -3,12 +3,12 @@
 #
 
 # Declare a variable and initialize it
-f=0 #creating a variable named "f" and setting equal to 0
+f= 0 #creating a variable named "f" and setting equal to 0
 print(f) #printing this variable.
 
 # re-declaring the variable works
-
-
+f= "abc" #we are re-declaring it again with this line.
+print(f)
 
 # ERROR: variables of different types cannot be combined
 
