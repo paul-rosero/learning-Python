e7bd18d2337c8cdd725d66c36ab3107b422f5b75
Paul Rosero <paul.rosero23@gmail.com>
2020-12-12 17:40:00 -0500

    explain what lines 14-15 do

--- Exercise Files/Ch2/helloworld_start.py
+++ Exercise Files/Ch2/helloworld_start.py
@@ -10,7 +10,7 @@ def main():
     print("hello world!")
     name = input("What is your name? ")
     print("Nice to meet you,", name)
-
+# checks to see if a function or module and then runs the code.
 if __name__ == "__main__":
     main()
 
