081fbd69180ff81bf87b4ade97fbb6c63f47c955
Paul Rosero <paul.rosero23@gmail.com>
2020-12-15 18:08:40 -0500

    add 2 more funcs to test out functions

--- Exercise Files/Ch2/functions_start.py
+++ Exercise Files/Ch2/functions_start.py
@@ -4,17 +4,22 @@
 
 # define a basic function
 #functions are objects that can be passed around to other pieces of python code. 
-def func1(): # the, : ,is scope definer
+def func1():            # the, : ,is scope definer
     print("I am a function")
-func1() # returns or prints "I am a function" only since thats what is what the function does. 
-print(func1()) # prints  "I am a function" first since thats what the func1 does, the the outer print() executes and since the func1 does not return a value, python evaluates the return value to be the constant of none and prints the string representation of none.
-print(func1) # this just prints the value of the function definition itself, which evaluates to: <function func1 at 0x7f89a8572ee0>
+
+#func1()           # returns or prints "I am a function" only since thats what is what the function does. 
+
+#print(func1())          # prints  "I am a function" first since thats what the func1 does, the the outer print() executes and since the func1 does not return a value, python evaluates the return value to be the constant of none and prints the string representation of none.
+
+#print(func1)            # this just prints the value of the function definition itself, which evaluates to: <function func1 at 0x7f89a8572ee0>
 
 # function that takes arguments
-
+def func2(arg1, arg2):
+    print(arg1, " ", arg2)
 
 # function that returns a value
-
+def cube(x):
+    return x * x * x
 
 # function with default value for an argument
 
