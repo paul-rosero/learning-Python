7681763f7f2f44b15f8581dc9756df770599dff3
Paul Rosero <paul.rosero23@gmail.com>
2020-12-22 17:49:34 -0500

    add simple while loop and simple for loop

--- Exercise Files/Ch2/loops_start.py
+++ Exercise Files/Ch2/loops_start.py
@@ -6,10 +6,13 @@ def main():
   x = 0
 
   # define a while loop
+  # while (x < 5):      #executes a block of code while a particular condition evaluates true
+  #   print(x) # while x is less than 5 it will print 
+  #   x = x + 1
 
-
-  # define a for loop
-
+  # define a for loop; these are iterators
+  for x in range(5,10):
+    print(x)
 
   # use a for loop over a collection
 
