cb066bb1c2ddbf2958f393cb3cfa078df52cf9ad
Paul Rosero <paul.rosero23@gmail.com>
2020-12-22 17:08:36 -0500

    write code and commented out to explain what it prints

--- Exercise Files/Ch2/conditionals_start.py
+++ Exercise Files/Ch2/conditionals_start.py
@@ -4,12 +4,35 @@
 
 
 def main():
-    x, y = 10, 100
-
+   
     # conditional flow uses if, elif, else
+    #x, y = 10, 100
+    #if ( x < y ):
+    #    st = "x is less than y"
+    #print(st)
+
+    #x, y = 1000, 100
+    #if ( x < y ):
+    #    st = "x is less than y"
+    #else:
+    #    st = "x is greater than y"
+    #print(st)
+
+    #x, y = 100, 100
+    #if ( x < y ):
+    #    st = "x is less than y"
+    #elif (x == y):
+    #    st = "x is the sae as y"
+    #else:
+    #    st = "x is greater than y"
+    #print(st)
 
     # conditional statements let you use "a if C else b"
-
+    # this is a consice way of writing the comparison logic in one line.
+    #similar to terniary 
+    x, y = 1000, 100
+    st = "x is less than y" if (x < y) else "x is greater than or equal to y"
+    print(st)
 
 if __name__ == "__main__":
     main()
